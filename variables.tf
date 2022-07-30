ebb534e050f3a3ee83a27134fe35004cf671cb6b
EC2 Default User <ec2-user@ip-172-31-8-66.eu-north-1.compute.internal>
2022-07-30 20:27:33 +0000

    initial

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "project_name" {
+  type    = string
+  default = "terraform-registry"
+}
+
+variable "region" {
+  type        = string
+  description = "region to deploy to"
+  default     = "us-east-1"
+}
+
+variable "cidr_block" {
+  type        = string
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
